a3b31f35803d7b67bc5036b7402009f7c72664bb
Alby001 <115143070+Alby001@users.noreply.github.com>
2023-01-08 15:18:26 +0100

    Step 4

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import { storyblokInit, apiPlugin } from "@storyblok/react";
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
+import* as ga from '..lib/ga'
+
 
 import Person from "../components/specificComponents/Person/Person";
 import Teacher from "../components/specificComponents/Teacher/Teacher";
@@ -78,4 +80,23 @@ function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+
+useEffect(() => {
+  const handleRouteChange = (url) => {
+    ga.pageview(url)
+  }
+  //When the component is mounted, subscribe to router changes
+  //and log those page views
+  router.events.on('routeChangeComplete', handleRouteChange)
+
+  // If the component is unmounted, unsubscribe
+  // from the event with the `off` method
+  return () => {
+    router.events.off('routeChangeComplete', handleRouteChange)
+  }
+}, [router.events])
+
+return <Component {...pageProps} />
+
+
+export default MyApp
